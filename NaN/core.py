75526d2c65a4de790a7d2af10ef5917a355501af
Jac <jackieliu1998@gmail.com>
2023-04-02 19:29:32 -0400

    fix some stuff

--- NaN/core.py
+++ NaN/core.py
@@ -1,7 +1,9 @@
 from ctypes import *
 import os
 
-lib = cdll.LoadLibrary("kernals/build/mat_gen.so")
+curr_dir = os.getcwd()
+path = os.path.join(curr_dir, 'kernals/build/mat_gen.so')
+lib = cdll.LoadLibrary(path)
 
 
 class object:
