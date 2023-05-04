d90a0fe925d03e65d1e9b6f4ba424d58171c0291
Liu Jiexiang <liujiexiang@Lius-MBP.lan>
2023-05-03 22:22:24 -0700

    update requirments

--- requirement.py
+++ requirement.py
@@ -1,12 +1,22 @@
 import ctypes, ctypes.util
 import subprocess as sp
 
+print("Installing Dependencies...")
 if ctypes.util.find_library('openblas') == None:
     print("BLAS library not found, installing...")
     sp.run(['conda', 'install', '-c', 'conda-forge', 'openblas'])
-    print("BLAS library installed")
+    if ctypes.util.find_library('openblas') == None:
+        print("BLAS installed failed, please install manually")
+        exit()
+    else:
+        print("BLAS library installed")
 else:
     print('CBLAS found')
+
+print("Installing Numpy for testing...")
+sp.run(['conda', 'install', 'numpy'])
+print("Installing Colorama...")
+sp.run(['conda', 'install -c conda-forge', 'colorama'])
 
 print("Setting up libraries...")
 if sp.run(["find", "kernals/build"], stdout=sp.PIPE).stdout.decode('utf-8') == '':
@@ -14,8 +24,11 @@ if sp.run(["find", "kernals/build"], stdout=sp.PIPE).stdout.decode('utf-8') == '
 sp.run(['cd', 'NaN/utils', '&&', 'pip install .'])
 sp.run(['cd ..'])
 sp.run(['cd ..'])
-sp.run(['conda', 'install', 'numpy'])
-sp.run(['conda', 'install -c conda-forge', 'colorama'])
 
-print('NaN is set up, runnning tests...')
-#sp.run(['python', 'test.py'])
+input("NaN installed, do you want to run the test? (y/n)")
+while input() != 'y' or input() != 'n':
+    input("Invalid input, please enter again (y/n): ")
+if input() == 'y':
+    sp.run(['python test.py'])
+else:
+    print("Test skipped")
