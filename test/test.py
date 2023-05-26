79e5c2ab22a6fb5873e52d85b2a2630e7dd6d304
Liu Jiexiang <liujiexiang@Lius-MacBook-Pro.local>
2023-05-26 16:09:34 -0400

    formal testing

--- test/test.py
+++ test/test.py
@@ -1,9 +1,10 @@
 from nan.lib import matgen as mg
+from nan.lib import ops as op
 from nan.matrix import matrix
 from colorama import Fore
 import numpy as np
+import numpy.linalg as la
 import time
-
 
 # font colors
 red = Fore.RED
@@ -11,113 +12,129 @@ green = Fore.GREEN
 success = green + 'Success' + Fore.RESET
 fail = red + 'Fail' + Fore.RESET
 
+class test:
+    def __init__(self) -> None:
+        self.t1 = time.monotonic()
+        self.t2 = None
+        self.normal = lambda i,j:mg.rand((i,j),(0,5))
+        self.eye = lambda i:mg.eye(i)
+        self.zeros = lambda i,j:mg.zeros((i,j))
+        self.randn = lambda i,j:mg.randn((i,j),(0,5))
+        self.rot2 = lambda i:mg.rot2(i)
+        self.rot3 = lambda i:mg.rot3(i, 2)
+        self.mat = lambda i,j,k:matrix([[i,j,k],[i,k,j],[j,k,i]])
+        self.N = 24
 
-t1 = time.monotonic()
-N = 2048
-# perform square matrix multiplication verifications
-# ===================================================================
-print('-' * 50)
-print('Performing matrix multiplication verifications')
-print('-' * 50)
-print('Generate random matrices and perform matrix multiplication and addition')
-A = mg.rand((N, N), (0, 1), 'double')
-B = mg.rand((N, N), (-5, 5), 'double')
-D = A+B+A
-C = A*B*A
-C = np.ctypeslib.as_array(C.data, (N,N))
-A = np.ctypeslib.as_array(A.data, (N,N))
-B = np.ctypeslib.as_array(B.data, (N,N))
-D = np.ctypeslib.as_array(D.data, (N,N))
-An = np.array(A, dtype=np.float64)
-Bn = np.array(B, dtype=np.float64)
-Cn = An @ Bn @ An
-Dn = An + Bn + An
-print(success if np.allclose(C, Cn) else fail)
-print(success if np.allclose(D, Dn) else fail)
-#assert np.allclose(C.data, Cn)
-print("Testing rotation matrix generation and operations")
-r = mg.rot2(45)
-i = mg.randn((2, 1), (0, 1), 'float')
-out = r*i
-Rn = np.ctypeslib.as_array(r.data, (2,2))
-In = np.ctypeslib.as_array(i.data, (2,1))
-out = np.ctypeslib.as_array(out.data, (2,1))
-outn = Rn @ In
-print(success if np.allclose(out, outn) else fail)
-print('-' * 50)
-# ===================================================================
-print('Testing other matrix generation')
-print('-' * 50)
-# testing zeros
-print('Testing zeros')
-z = mg.zeros((N, N), 'double')
-zn = np.ctypeslib.as_array(z.data, (N,N))
-print(success if np.allclose(zn, np.zeros((N, N))) else fail)
-print('Testing eye')
-identity = mg.eye(N, 'double')
-identity = np.ctypeslib.as_array(identity.data, (N,N))
-print(success if identity.all()==np.eye(N).all() else fail)
-print("Testing Non-Squared matrix multiplication")
-N1 = 100
-N2 = 200
-a = mg.randn((N1,N2), (0,1), 'double')
-b = mg.randn((N2,N1), (0,1), 'double')
-c = a*b*a
-a = np.ctypeslib.as_array(a.data, (N1,N2))
-b = np.ctypeslib.as_array(b.data, (N2,N1))
-c = np.ctypeslib.as_array(c.data, (N1,N2))
-an = np.array(a)
-bn = np.array(b)
-cn = an @ bn @ an
-print(success if np.allclose(c, cn) else fail)
-print("Testing float type matrices operation")
-a = mg.rand((N1,N2), (0,5), 'float')
-b = mg.rand((N2,N1), (0,5), 'float')
-c = a*b
-a = np.ctypeslib.as_array(a.data, (N1,N2))
-b = np.ctypeslib.as_array(b.data, (N2,N1))
-c = np.ctypeslib.as_array(c.data, (N1,N1))
-an = np.array(a)
-bn = np.array(b)
-cn = an @ bn
-print(success if np.allclose(c, cn) else fail)
+    def test_matmul(self):
+        title = "Testing matrix multiplication"
+        for i in range(1,self.N):
+            for j in range(1,self.N):
+                a = self.normal(i,j)
+                b = self.normal(j,i)
+                c = a*b
+                an = np.ctypeslib.as_array(a.data, (i,j))
+                bn = np.ctypeslib.as_array(b.data, (j,i))
+                cn = np.ctypeslib.as_array(c.data, (i,i))
+                assert np.allclose(cn, an @ bn)
+        print(title, success)
 
-print('-' * 50)
-print('Testing Rotation matrix')
-print('-' * 50)
-v = matrix([[1],[1]], 'float')
-v2 = matrix([[1],[1],[1]], 'float')
-v_test = matrix([0,1], 'float')
-r2 = mg.rot2(60)
-r3 = mg.rot3(60, 0)
-v = r2*v
-v2 = r3*v2
+    def test_matadd(self):
+        title = "Testing matrix addition"
+        for i in range(1,self.N):
+            for j in range(1,self.N):
+                a = self.randn(i,j)
+                b = self.normal(i,j)
+                c = a+b
+                an = np.ctypeslib.as_array(a.data, (i,j))
+                bn = np.ctypeslib.as_array(b.data, (i,j))
+                cn = np.ctypeslib.as_array(c.data, (i,j))
+                assert np.allclose(cn, an + bn)
+        print(title, success)
 
-r2 = np.ctypeslib.as_array(r2.data, (2,2))
-r3 = np.ctypeslib.as_array(r3.data, (3,3))
-v = np.ctypeslib.as_array(v.data, (2,1))
-v2 = np.ctypeslib.as_array(v2.data, (3,1))
+    def test_matmuladd(self):
+        title = "Testing matrix multiplication and addition"
+        for i in range(1,self.N):
+            for j in range(1,self.N):
+                a = self.randn(i,j)
+                b = self.normal(j,i)
+                c = self.randn(i,i)
+                d = a*b+c
+                an = np.ctypeslib.as_array(a.data, (i,j))
+                bn = np.ctypeslib.as_array(b.data, (j,i))
+                cn = np.ctypeslib.as_array(c.data, (i,i))
+                dn = np.ctypeslib.as_array(d.data, (i,i))
+                assert np.allclose(dn, an @ bn + cn)
+        print(title, success)
 
-print(success if np.allclose(v, v2[1:]) else fail)
-r2 = mg.rot2(30, "double")
-r3 = mg.rot3(30, 1, "double")
-r2 = np.ctypeslib.as_array(r2.data, (2,2))
-r3 = np.ctypeslib.as_array(r3.data, (3,3))
+    def test_matmuladdsub(self):
+        title = "Testing matrix multiplication, addition and subtraction"
+        for i in range(1,self.N):
+            for j in range(1,self.N):
+                a = self.randn(i,j)
+                b = self.normal(j,i)
+                c = self.randn(i,i)
+                d = self.randn(i,i)
+                e = a*b+c-d
+                an = np.ctypeslib.as_array(a.data, (i,j))
+                bn = np.ctypeslib.as_array(b.data, (j,i))
+                cn = np.ctypeslib.as_array(c.data, (i,i))
+                dn = np.ctypeslib.as_array(d.data, (i,i))
+                en = np.ctypeslib.as_array(e.data, (i,i))
+                assert np.allclose(en, an @ bn + cn - dn)
+        print(title, success)
 
-r3 = matrix([[r3[0][0], r3[2][0]],[r3[0][2],r3[2][2]]], 'double')
-r3 = np.ctypeslib.as_array(r3.data, (2,2))
-print(success if np.allclose(r3, r2) else fail)
-print('-' * 50)
-r4 = r3.copy()
-r5 = np.ctypeslib.as_array(r4.data, (2,2))
-print(success if np.allclose(r5, r4) else fail)
+    def test_eigendecomp(self):
+        title = "Testing eigen decomposition"
+        for i in range(2,self.N):
+            for j in range(2,self.N):
+                for k in range(2, self.N):
+                    ma = self.mat(i,j,k)
+                    w,v = op.eig(ma)
+                    an = np.ctypeslib.as_array(ma.data, (3,3))
+                    wn = np.ctypeslib.as_array(w.data, (3,))
+                    vn = np.ctypeslib.as_array(v.data, (9,)).reshape(3,3)
+                    wnn, vnn = la.eig(an)
+                    wn = np.sort(wn); wnn = np.sort(wnn)
+                    vn = la.norm(np.sort(vn, axis=0), axis=1); vnn = la.norm(np.sort(vnn, axis=0), axis=1)
+                    assert np.allclose(wn, wnn.real, atol=1e-2)
+        print(title, success)
 
-N2 = 100
-A = mg.randn((N2, N2), (0, 1), 'double')
-B = A[0:5, 0:5]
-A = np.ctypeslib.as_array(A.data, (N2,N2))
-B = np.ctypeslib.as_array(B.data, (5,5))
-Bn = A[0:5, 0:5]
-print(success if B.all()==Bn.all() else fail)
-t = time.monotonic() - t1
-print(f"Test completed in {t:.2f} seconds")
+    def test_svd(self):
+        title = "Testing singular value decomposition"
+        for i in range(2,self.N):
+            for j in range(2,self.N):
+                a = self.randn(i,j)
+                u,s,v = op.svd(a)
+                an = u*s*v
+                a = np.ctypeslib.as_array(a.data, (i,j))
+                an = np.ctypeslib.as_array(an.data, (i,j))
+                assert np.allclose(a, an, atol=1e-3)
+        print(title, success)
+
+    def test_transpose(self):
+        title = "Testing transpose"
+        for i in range(2,self.N):
+            for j in range(2,self.N):
+                a = self.randn(i,j)
+                b = a.T()
+                an = np.ctypeslib.as_array(a.data, (i,j))
+                bn = np.ctypeslib.as_array(b.data, (j,i))
+                assert np.allclose(an.T, bn)
+        print(title, success)
+
+    def test_all(self):
+        self.test_matmul()
+        self.test_matadd()
+        self.test_matmuladd()
+        self.test_matmuladdsub()
+        self.test_eigendecomp()
+        self.test_svd()
+        self.test_transpose()
+
+
+if __name__ == "__main__":
+    t = test()
+    t.test_all()
+
+                
+        
