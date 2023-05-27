45ff1c0a22e6dc019a340c0f12cfa5d32cd32183
Liu Jiexiang <liujiexiang@Lius-MacBook-Pro.local>
2023-05-26 22:16:28 -0400

    eig shape

--- nan/lib.py
+++ nan/lib.py
@@ -73,8 +73,8 @@ class ops(matrix):
             raise TypeError('Non-square matrix cannot be eigendecomposed')
         func = ALLType.LapackDict['eig'][in_matrix.dtype]
         wr, wi, ev = func(in_matrix.core.data, in_matrix.shape)
-        return (matrix(object(wr, in_matrix.shape, in_matrix.dtype), in_matrix.dtype), 
-                matrix(object(wi, in_matrix.shape, in_matrix.dtype), in_matrix.dtype),
+        return (matrix(object(wr, (1,in_matrix.shape[0]), in_matrix.dtype), in_matrix.dtype), 
+                matrix(object(wi, (1,in_matrix.shape[0]), in_matrix.dtype), in_matrix.dtype),
                 matrix(object(ev, in_matrix.shape, in_matrix.dtype), in_matrix.dtype))
     
     @staticmethod
